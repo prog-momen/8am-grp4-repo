2e2a28dd7c7402fe62c68fec64982bb22e7a7c63
prog-momen <s12325799@stu.najah.edu>
2024-12-08 13:33:12 +0200

    32

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Clinic1 from './components/clinics components'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+<Clinic1/>
+</div>
   );
-}
+};
 
 export default App;
